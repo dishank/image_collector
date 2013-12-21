d47b34269dcb6f694e20c431fa6bcf0d40d52755
Dishank Patel <dishankptl@gmail.com>
2013-12-21 15:11:59 -0500

    First Commit of image_collector from the different webservers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageCollector::Application.config.secret_key_base = '04906ac53abdef2bbb947a4569f30a67255f460e00b247a9f7bc842c0bc31515ee1196e18887191eb3799dc6bbfa2a16d964493ed22b1bb8b04f322426ee9ff0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImageCollector::Application.config.session_store :cookie_store, key: '_image_collector_session'
